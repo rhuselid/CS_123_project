c8f617b6d3d9a198c33543483a71d292aa40abe1
Robert Huselid <rhuselid@cs.uchicago.edu>
2019-05-10 15:23:17 -0500

    added lat long conditional

--- code/sentiment_analysis_mapreduce.py
+++ code/sentiment_analysis_mapreduce.py
@@ -2,8 +2,9 @@ from mrjob.job import MRJob
 import json
 import nltk
 import re
+
 #nltk.download('stopwords')
-nltk.download('vader_lexicon')
+#nltk.download('vader_lexicon')
 from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 stop_words=set(stopwords.words("english"))
@@ -21,26 +22,37 @@ class AnalyzeSentiment(MRJob):
         #print(line.keys())
         #time = line['timestamp_ms']
         if ('geo' in line.keys()) and (line['geo']):
+            top = 49.3457868 # north lat
+            left = -124.7844079 # west long
+            right = -66.9513812 # east long
+            bottom =  24.7433195 # south lat
+            # above continental us borders pulled from: https://gist.github.com/jsundram/1251783
+
             lat = line['geo']['coordinates'][0]
             lon = line['geo']['coordinates'][1]
+            print(lat, lon)
             
-            if 'text' in line.keys():
-                text = line['text']
-                filtered = ''
-                for word in WORD_RE.findall(text):
-                    if word not in stop_words:
-                        if filtered:
-                            filtered += ' '
-                        filtered += str(word)
+            if (bottom < lat and top > lat) and (left < lon and right > lon):
+                if 'text' in line.keys():
+                    text = line['text']
+                    filtered = ''
+                    for word in WORD_RE.findall(text):
+                        if word not in stop_words:
+                            if filtered:
+                                filtered += ' '
+                            filtered += str(word)
+                    print(filtered)
+                    sentiment = SentimentIntensityAnalyzer().polarity_scores(filtered)#['pos']
+                    
+                    yield [lat, lon], sentiment
 
-                sentiment = SentimentIntensityAnalyzer().polarity_scores(filtered)['pos']
-                yield [lat, lon], sentiment
 
     def combiner(self, location, sentiment):
-        yield location, sentiment
+        yield location, list(sentiment)
 
-    def reducer(self, word, sentiment):
-        yield location, sentiment
+
+    def reducer(self, location, sentiment):
+        yield location, list(sentiment)
 
 
 if __name__ == '__main__':
