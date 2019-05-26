032dcde133b6e35727613a5ccf6a80a8d0a13628
ksayid <30742855+ksayid@users.noreply.github.com>
2019-05-26 00:02:46 -0500

    Update unzipper.sh

--- code/unzipper.sh
+++ code/unzipper.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 # All tar files should be in a folder.
-# This code unpacks each tar file in the folder
-for f in `ls *.tar` ; do
-    dir="${f%.*}"
-    mkdir "$dir"
-    cd $dir
-    tar -xf "../$f"
-    cd ..
-done
+# This code unpacks each tar file in the folder and deletes the tar file/ 
+for file in `ls *.tar`; do tar xf "${file}" && rm "${file}"; done
+# This code unzips all of the bz2's in each folder into jsons. 
+find -type f -name "*.json.bz2" -exec bunzip2 *.bz2 {} \; 
 
-# This code enters each folder (made from each tar), navigates down 4 levels, and unzips each bz2 file
-for d in ./*/ ; do
-    cd *; cd *; cd *; cd *
-    for j in ./*/ ; do
-      (cd "$j" && bunzip2 *.bz2) done
-done
+for i in file1 file2 file3 file4 ; do cat "$i" >> output && rm "$i" || break ; done
 
+find . -type f -exec cat {} + > all.json;
 
 
