2702c0ca5058d55f73de3f2b1c0306f974441fd7
ksayid <30742855+ksayid@users.noreply.github.com>
2019-05-26 00:33:28 -0500

    Update unzipper.sh

--- code/unzipper.sh
+++ code/unzipper.sh
@@ -5,8 +5,13 @@ for file in `ls *.tar`; do tar xf "${file}" && rm "${file}"; done
 # This code unzips all of the bz2's in each folder into jsons. 
 find -type f -name "*.json.bz2" -exec bunzip2 *.bz2 {} \; 
 
-for i in file1 file2 file3 file4 ; do cat "$i" >> output && rm "$i" || break ; done
+# for i in ($find . -type f ! -name "all.json") ; do cat "$i" >> all.json && rm "$i" || break ; done
+# find . -type f ! -name "all.json" -exec cat {} + ; -exec rm {} \;
 
 find . -type f -exec cat {} + > all.json
+bash -O extglob
+rm -- !(all.json)
+
+
 
 
