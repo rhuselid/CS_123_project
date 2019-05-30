3649c4dd08300ecb2bb81abfd68658d1a8410e02
Pranab Islam <islampranab@gmail.com>
2019-05-30 17:40:53 -0500

    small updates. getting json encoding nonsense

--- code/MR_time_series.py
+++ code/MR_time_series.py
@@ -18,30 +18,38 @@ WORD_RE = re.compile(r"[\w']+")
 class TimeSeries(MRJob):
 
     def mapper(self, _, line):
-        line = json.loads(line)
-        filtered = ''
         
-        if ts.is_tweet_of_interest(line):
-            user_id = line['user']['id']
-            tweet_id = line['id']
-            time_stamp = datetime.strptime(line['created_at'], '%a %b %d %H:%M:%S %z %Y')
-            time_stamp = datetime.timestamp(time_stamp)
-            tweet_text = line['text']
-            print("about to score sentiments")
-            
-            for word in WORD_RE.findall(tweet_text):
-                if word not in stop_words:
-                    if filtered:
-                        filtered += ' '
-                    filtered += str(word)
-            sentiment = SentimentIntensityAnalyzer().polarity_scores(filtered)['compound']
-            print("about to yield")
-            value_string = str(tweet_id) + '|' + str(sentiment) + '|' + str(time_stamp)
-            yield user_id, value_string
 
-        # else:
-        #     ## Problem with my if statement so yield something of same structure
-        #     yield random.randint(0,12), random.randint(0,234)
+        try:
+
+            line = json.loads(line)
+            filtered = ''
+            
+            if ts.is_tweet_of_interest(line):
+                user_id = line['user']['id']
+                tweet_id = line['id']
+                time_stamp = datetime.strptime(line['created_at'], '%a %b %d %H:%M:%S %z %Y')
+                time_stamp = datetime.timestamp(time_stamp)
+                tweet_text = line['text']
+                print("about to score sentiments")
+                
+                for word in WORD_RE.findall(tweet_text):
+                    if word not in stop_words:
+                        if filtered:
+                            filtered += ' '
+                        filtered += str(word)
+                sentiment = SentimentIntensityAnalyzer().polarity_scores(filtered)['compound']
+                print("about to yield")
+                value_string = str(tweet_id) + '|' + str(sentiment) + '|' + str(time_stamp)
+                yield user_id, value_string
+
+        except Exception as e:
+            print()
+            print("WE have an exception here it is:")
+            print(e)
+            print(line)
+
+
 
 
     # def combiner(self, location, sentiment):
@@ -49,10 +57,10 @@ class TimeSeries(MRJob):
 
 
     def reducer(self, users, tweets_sentiments):
-        print("arrived to reducer")
+        # print("arrived to reducer")
 
         ## No repeats
-        set_tweets_sentiments = list(set(tweets_sentiments))
+        tweets_sentiments = list(set(tweets_sentiments))
         # print(set_tweets_sentiments)
         # print(set_tweets_sentiments)
 
@@ -60,7 +68,7 @@ class TimeSeries(MRJob):
 
         ## STILL HAVE TO ADD THE TIME TO EACH SENTIMENT
 
-        yield users, set_tweets_sentiments
+        yield users, tweets_sentiments
 
 
 if __name__ == '__main__':
