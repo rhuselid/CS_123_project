4131f28da04f43ce75c4eea8b48284f1a45539cf
Pranab Islam <islampranab@gmail.com>
2019-06-01 00:57:26 -0500

    edited file path

--- code/MR_Compare_Users.py
+++ code/MR_Compare_Users.py
@@ -11,7 +11,7 @@ class CompareUsers(MRJob):
         user_id = list(line.keys())[0]
         beta = line[user_id]
 
-        with open("/home/student/CS_123_project/code/all_user_betas_small_copy.json") as f:
+        with open("/home/student/CS_123_project/code/all_user_betas_copy.json") as f:
             for line2 in f:
                 line2 = json.loads(line2)
 
