61b8818b24733f2d4c98b9204852314e2fa1759c
ksayid <30742855+ksayid@users.noreply.github.com>
2019-06-05 15:34:24 -0500

    Update cluster_comparison_visuals.py

--- code/cluster_comparison_visuals.py
+++ code/cluster_comparison_visuals.py
@@ -31,31 +31,33 @@ with open("cluster_comparison_final2.txt", "r") as f:
 # Thus, we neglect to fix the 0.1% of tweets that have errors. 
 
 # Graph the data! 
+
 for user in d.keys():
-  home_x, home_y = d[user][0][2], d[user][0][1]
-  similar_x = list(chain.from_iterable(d[user][i][2] for i in range(1,4)))
-  similar_y = list(chain.from_iterable(d[user][i][1] for i in range(1,4)))
+    home_x, home_y = d[user][0][2], d[user][0][1]
+    similar_x = list(chain.from_iterable(d[user][i][2] for i in range(1,len(list(d[user])))))
+    similar_y = list(chain.from_iterable(d[user][i][1] for i in range(1,len(list(d[user])))))
 
-  map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
-          projection='lcc',lat_1=32,lat_2=45,lon_0=-95)
+    map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49, 
+                  projection='lcc',lat_1=32,lat_2=45,lon_0=-95)
 
-  #The first parameter is the location of the 3 st99_d00 files. 
-  map.readshapefile(r'st99_d00', name='states', drawbounds=True)
+    #The first parameter is the location of the 3 st99_d00 files. 
+    map.readshapefile(r'/st99_d00', name='states', drawbounds=True)
 
-  new_x,new_y = map(home_x,home_y)
-  map.plot(new_x,new_y,marker='o',color='blue',markersize=5, label = "User")
+    new_x,new_y = map(home_x,home_y)
+    map.plot(new_x,new_y,marker='o',color='blue',markersize=5, label = "User")
   
-  flag = True
+    flag = True
   
-  for (x,y) in zip(similar_x, similar_y):
-      new_x, new_y = map(x,y)
-      if flag:
-          map.plot(new_x,new_y,marker='o',color='orange',markersize=5, label = "Similar Users")
-          flag = False
-          continue
-      map.plot(new_x,new_y,marker='o',color='orange',markersize=5)
-  
-  plt.legend(loc='upper left')
-  userid = str(user)
-  filename = "analysis_" + str(user) + ".png"
-  plt.savefig(filename)
+    for (x,y) in zip(similar_x, similar_y):
+        new_x, new_y = map(x,y)
+        if flag:
+            map.plot(new_x,new_y,marker='o',color='orange',markersize=5, label = "Similar Users")
+            flag = False
+            continue
+        map.plot(new_x,new_y,marker='o',color='orange',markersize=5)
+
+    plt.legend(loc='upper left')
+    userid = str(user)
+    filename = "analysis_" + str(user) + ".png"
+    plt.savefig(filename)
+    plt.clf()
